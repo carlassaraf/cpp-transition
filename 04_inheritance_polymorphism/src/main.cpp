a826062e8c0b982d75ffb3a576d0bc286f749859
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-05-22 10:03:40 -0300

    feat: polymorfism assignment

--- 04_inheritance_polymorphism/src/main.cpp
+++ 04_inheritance_polymorphism/src/main.cpp
@@ -21,6 +21,14 @@ public:
     virtual ~Sensor() = default;
     virtual float read() = 0;
     virtual const char *name() const = 0;
+    bool is_above_threshold() {
+        return read() > threshold_;
+    }
+    void set_threshold(float thres) {
+        threshold_ = thres;
+    }
+protected:
+    float threshold_;
 };
 
 /* =========================================================================
@@ -33,14 +41,18 @@ class SimulatedTemperatureSensor : public Sensor {
 public:
     SimulatedTemperatureSensor() : temperature_(20.0f) {}
 
+    // Default destructor is generated
+
     float read() override {
-        /* TODO */
-        return 0.0f;
+        temperature_ += 0.5f;
+        if (temperature_ > 30.0f) {
+            temperature_ = 20.0f;
+        }
+        return temperature_;
     }
 
     const char *name() const override {
-        /* TODO */
-        return "";
+        return "TempSensor";
     }
 
 private:
@@ -58,17 +70,22 @@ private:
 class SimulatedButtonSensor : public Sensor {
 public:
     explicit SimulatedButtonSensor(const gpio_dt_spec &spec) : spec_(spec) {
-        /* TODO: configure GPIO */
+        if(!gpio_is_ready_dt(&spec_)) {
+            printk("Error: GPIO device not ready\n");
+            return;
+        }
+        gpio_pin_configure_dt(&spec_, GPIO_INPUT);
     }
 
+    // Default constructor is generated
+
     float read() override {
-        /* TODO */
-        return 0.0f;
+        // Active-low
+        return (gpio_pin_get_dt(&spec_) == 1) ? 1.0f : 0.0f;
     }
 
     const char *name() const override {
-        /* TODO */
-        return "";
+        return "ButtonSensor";
     }
 
 private:
@@ -85,6 +102,9 @@ int main(void)
     SimulatedTemperatureSensor temp;
     SimulatedButtonSensor button{kBtn0};
 
+    temp.set_threshold(25.0);
+    button.set_threshold(0.0);
+
     /* Polymorphic array — works for any Sensor subclass */
     Sensor *sensors[] = {&temp, &button};
 
@@ -95,6 +115,7 @@ int main(void)
                    s->name(),
                    static_cast<int>(val),
                    static_cast<int>((val - static_cast<int>(val)) * 10));
+            printk("%s\n", s->is_above_threshold()? "Over threshold" : "Not over threshold");
         }
 
         gpio_pin_toggle_dt(&kLed0);
